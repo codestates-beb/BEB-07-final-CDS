9c02b8d4068c80d201b967c99ca7a2056ffe26d4
Duhoon <eclip6@naver.com>
2023-01-18 23:37:44 +0900

    test: Github Actions 작동 확인

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <input className="premium" />
     </div>
   );
 }
