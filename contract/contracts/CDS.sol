f331797bb7f2797748bd5258682a0d552e40464f
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-01-19 22:33:42 +0900

    Fix: args of makeSwap

--- contract/contracts/CDS.sol
+++ contract/contracts/CDS.sol
@@ -1,45 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import './Swap/Swaps.sol';
+import './Swaps/Swaps.sol';
 
 contract CDS is Swaps {
   event MakeSwap(
     address indexed buyer,
-    uint256 numBlocks,
     uint256 claimPrice,
     uint256 liquidationPrice,
+    uint256 _premium,
     uint256 expirationMonth
   );
+  event AcceptSwap(address indexed seller, uint256 swapId);
 
   function makeSwap(
-    address _addr,
-    uint256 _numBlocks,
-    uint256 _claimRate,
-    uint256 _liquidationRate,
-    uint256 _expirationMonth
+    address addr,
+    uint256 claimPrice,
+    uint256 liquidationPrice,
+    uint256 sellerDeposit,
+    uint256 premium,
+    uint256 expirationMonth
   ) public returns (uint256) {
     uint256 newSwapId = _makeSwap(
-      _addr,
-      _numBlocks,
-      _claimRate,
-      _liquidationRate,
-      _expirationMonth
+      addr,
+      claimPrice,
+      liquidationPrice,
+      sellerDeposit,
+      premium,
+      expirationMonth
     );
-    emit MakeSwap(
-      _addr,
-      _numBlocks,
-      _claimRate,
-      _liquidationRate,
-      _expirationMonth
-    );
+    emit MakeSwap(addr, claimPrice, liquidationPrice, premium, expirationMonth);
     return newSwapId;
   }
 
-  // function acceptSwap(address _addr) public returns (uint256) {
-  // }
+  function acceptSwap(address addr, uint256 swapId) public returns (uint256) {
+    uint256 acceptedSwapId = _acceptSwap(addr, swapId);
+    emit AcceptSwap(addr, swapId);
+    return acceptedSwapId;
+  }
 
   function swap(uint256 swapId) public view returns (Swap memory) {
     return _swaps[swapId];
+  }
+
+  function buyer(uint256 swapId) public view returns (Buyer memory) {
+    return _swaps[swapId].buyer;
+  }
+
+  function seller(uint256 swapId) public view returns (Seller memory) {
+    return _swaps[swapId].seller;
   }
 }
