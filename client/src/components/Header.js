3c27a624b832fa594a10e68c25c11a20bd0eb764
Duhoon <eclip6@naver.com>
2023-01-23 14:37:02 +0900

    feat: API 요청 모듈 추가 및 axios 라이브러리 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,23 +6,23 @@ import '../assets/css/header.css'
 
 function Header() {
   return (
-    <div className="header py-2 px-4 flex justify-between">
-      <div className="header-logo-wrapper">
-        <Link link="/" className="logo-link">
-            <img className="service-logo" />
-            <div className="service-name">
+    <div className='header py-2 px-4 flex justify-between'>
+      <div className='header-logo-wrapper'>
+        <Link link="/" className='logo-link'>
+            <img className='service-logo' />
+            <div className='service-name'>
               <p>Crypto</p>
               <p>Default Swap</p>
             </div>
         </Link>
       </div>
       <div className="navbar flex items-center">
-        <ul className="navbar-wrapper flex">
-          <Link><li className="navbar-item">MyPage</li></Link>
-          <Link><li className="navbar-item">About</li></Link>
-          <Link><li className="navbar-item">Create CDS</li></Link>
+        <ul className='navbar-wrapper flex'>
+          <Link><li className='navbar-item'>MyPage</li></Link>
+          <Link><li className='navbar-item'>About</li></Link>
+          <Link><li className='navbar-item'>Create CDS</li></Link>
         </ul>
-        <button>Connect Wallet</button>
+        <button className='navbar-button'>Connect Wallet</button>
       </div>
     </div>
   )
