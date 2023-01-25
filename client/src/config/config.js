fdc668e385e3e2f59c9def71052ba2d3f92ff59c
Duhoon <eclip6@naver.com>
2023-01-26 08:58:29 +0900

    fix: 최신 CDS 컨트랙트 적용

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,5 @@
 export default {
     devURL: "https://nodeauction.42msnsnsfoav6.ap-northeast-2.cs.amazonlightsail.com/dev",
 
-    contractAddr: "0xAE3b465F19Bf5B0B2ef94A1acFd41b412b1D36b2",
+    contractAddr: "0x4758213ffaD552EE16435f003e409b2e9dF65D57",
 }
