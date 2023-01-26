cd9655d46b42b278259c37a134564102c0fe9225
SeolDonghun <ssalssi1@gmail.com>
2023-01-26 16:55:46 +0900

    Feat: Update DB model and migration files

--- server/sequelizeMigration.sh
+++ server/sequelizeMigration.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+npx sequelize-cli db:migrate:undo:all --config ./src/config/dbconfig.js --migrations-path ./src/migrations/
 npx sequelize-cli db:migrate --config ./src/config/dbconfig.js --migrations-path ./src/migrations
