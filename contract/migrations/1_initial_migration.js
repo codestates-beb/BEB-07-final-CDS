16e518fda83e0cdd6d9e59d734ad3d8977d3f697
SeolDonghun <ssalssi1@gmail.com>
2023-01-27 12:00:52 +0900

    Feat: Add Oracle CA to migration result txt file

--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -4,12 +4,20 @@ const PriceOracleMock = artifacts.require('PriceOracleMock');
 const fs = require('fs');
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(PriceOracleMock, 20000);
-  const receipt = await deployer.deploy(CDS);
-  const txHash = receipt.transactionHash;
+  const oracleReceipt = await deployer.deploy(PriceOracleMock, 20000);
+  const CDSreceipt = await deployer.deploy(CDS);
   const currentTime = new Date();
   fs.writeFileSync(
     './CDS_CA.txt',
-    `${currentTime}\n \nThe Latest ADDRESS of CDS deployed on REMOTE network\n \nADDRESS:  ${CDS.address}\n \nTX HASH:  ${txHash}`,
+    `${currentTime}
+The Latest ADDRESS of CDS deployed on REMOTE network
+
+    CDS ADDRESS:  ${CDS.address}
+    TX HASH:  ${CDSreceipt.transactionHash}
+
+The Latest ADDRESS of PriceOracle deployed on REMOTE network
+
+    Oracle ADDRESS:  ${PriceOracleMock.address}`,
+    // TX HASH:  ${oracleReceipt.transactionHash}`,
   );
 };
