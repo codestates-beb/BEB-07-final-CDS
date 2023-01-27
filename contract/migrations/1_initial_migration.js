15f88d6b4adea690926c6f12ff49864c4aefaf27
SeolDonghun <ssalssi1@gmail.com>
2023-01-27 09:51:45 +0900

    Feat: Add txhash info to contract discord webhook

--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -5,10 +5,11 @@ const fs = require('fs');
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(PriceOracleMock, 20000);
-  await deployer.deploy(CDS);
+  const receipt = await deployer.deploy(CDS);
+  const txHash = receipt.transactionHash;
   const currentTime = new Date();
   fs.writeFileSync(
     './CDS_CA.txt',
-    `${currentTime}\n \nThe Newest ADDRESS of CDS deployed on REMOTE network\n \n${CDS.address}`,
+    `${currentTime}\n \nThe Latest ADDRESS of CDS deployed on REMOTE network\n \nADDRESS:  ${CDS.address}\n \nTX HASH:  ${txHash}`,
   );
 };
