3d5e04054f408776df9727db4556fe45a9e7f7a1
NotoriousHong <hjs97040@gmail.com>
2023-01-27 20:38:30 +0900

    edit: APIs and Logo

--- client/src/components/ProposedCard.js
+++ client/src/components/ProposedCard.js
@@ -1,6 +1,15 @@
-import BTHLogo from '../img/BTC_Logo.png';
+import BTHLogo from '../assets/img/BTC_Logo.png';
 
 function ProposedCard(props) {
+  function getSymbol(symbol) {
+    const symbolURL = `https://assets.coincap.io/assets/icons/${symbol}@2x.png`;
+    return symbolURL;
+  }
+
+  const btcSymbol = getSymbol('btc');
+  const ethSymbol = getSymbol('eth');
+  const dogeSymbol = getSymbol('doge');
+
   return (
     <div className="card bg-darkGrayColor w-[13rem] h-[17rem] rounded-3xl hover:scale-110 transition-all">
       <div className="mx-[1.5rem] py-[1.5rem]">
@@ -9,7 +18,7 @@ function ProposedCard(props) {
             <img
               className="w-10 h-10 bg-white"
               alt="BitcoinLogo"
-              src={BTHLogo}
+              src={btcSymbol}
             />
           </div>
           <div className="cds-type my-[0.4rem] font-semibold text-2xl">
@@ -34,7 +43,11 @@ function ProposedCard(props) {
           </div>
           <div className="body-content">
             <h3 className="content-name text-base font-medium">Expire Date</h3>
-            <p className="content text-sm font-light">{props.premiumRounds}</p>
+            <p className="content text-sm font-light">
+              {(Math.floor((props.premiumInterval / 604800) * 100) / 100) *
+                props.premiumRounds}{' '}
+              weeks later
+            </p>
           </div>
         </div>
       </div>
