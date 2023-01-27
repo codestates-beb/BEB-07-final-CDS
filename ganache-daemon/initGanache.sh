38704da3bc1008e41c6092c77156d1616ddab1e5
SeolDonghun <ssalssi1@gmail.com>
2023-01-27 11:39:57 +0900

    Feat: add ganache daemon

---
+++ ganache-daemon/initGanache.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+touch out.log
+ganache-cli --host 0.0.0.0 --port 8545 --networkId 5777 \
+-d suit there curtain unfold traffic brick \
+one genre holiday include bring thumb \
+--db database
