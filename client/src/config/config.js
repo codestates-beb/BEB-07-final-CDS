e390ed265cb2432bb3463e464b5674029650fed7
Duhoon <eclip6@naver.com>
2023-02-05 01:00:58 +0900

    fix: Create 기능 제공 최신 컨트랙트 반영

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -2,7 +2,7 @@ export default {
   devURL:
     'https://nodeauction.42msnsnsfoav6.ap-northeast-2.cs.amazonlightsail.com/dev',
 
-  contractAddr: '0x9f30a2763b6Ba8d4a31474240FCe170e7B746E45',
+  contractAddr: '0x7BfeBb3e6f3bCf4890c8A2fde0b3e2675B410B9f',
   oracleAddr: '0x2aC585EE9D26c614F8EB56007cecdbCC57B3Fbc2',
 
   // market price API
