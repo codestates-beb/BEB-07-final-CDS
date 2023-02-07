784372a93a8181603683722c29c8b7b2750a7180
SeolDonghun <ssalssi1@gmail.com>
2023-02-07 23:34:24 +0900

    Feat: Add routers for table JOIN queries

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -9,8 +9,12 @@ import redisClient from '../utils/redisClient';
 import { isValidEmail } from '../utils/inputValidators';
 import { AppDataSource } from '../data-source';
 import { Users } from '../entities/Users';
+import { Swaps } from '../entities/Swaps';
+import { Transactions } from '../entities/Transactions';
 
 const userRepository = AppDataSource.getRepository(Users);
+const swapRepository = AppDataSource.getRepository(Swaps);
+const transactionRepository = AppDataSource.getRepository(Transactions);
 
 const userController = {
   postMine: async (req: Request, res: Response, next: NextFunction) => {
@@ -37,11 +41,110 @@ const userController = {
       next(err);
     }
   },
+  getMySwaps: async (req: Request, res: Response, next: NextFunction) => {
+    const address = await redisClient.get(req.cookies.sessionID);
+    const user = await userRepository.findOneBy({ address });
+    if (!user) return res.status(403).json('no such user');
+    try {
+      let mySwaps = await swapRepository.findAndCount({
+        where: [{ seller: address }, { buyer: address }],
+      });
+      const result = {
+        address: address,
+        totalSwapCount: mySwaps[1],
+        swaps: mySwaps[0],
+      };
+      return res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  },
+  getMyTransactions: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const address = await redisClient.get(req.cookies.sessionID);
+      const user = await userRepository.findOneBy({ address });
+      if (!user) return res.status(403).json('no such user');
+      let txQuery = transactionRepository
+        .createQueryBuilder('transactions')
+        .select('transactions')
+        .innerJoin(Swaps, 'swaps', 'swaps.swapId = transactions.swapId')
+        .innerJoin(
+          Users,
+          'users',
+          'users.address = swaps.seller OR users.address = swaps.buyer',
+        )
+        .where(`users.address = "${address}"`);
+      const result = await txQuery.getManyAndCount();
+      return res.status(200).json({
+        address,
+        totaltransactionCount: result[1],
+        transactions: result[0],
+      });
+    } catch (err) {
+      console.error(err);
+      return next(err);
+    }
+  },
   getByAddress: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const address = req.params.address;
-      const singleUser = await userRepository.findOneBy({ address });
-      return res.status(200).json(singleUser);
+      const user = await userRepository.findOneBy({ address });
+      if (!user) return res.status(403).json('no such user');
+      return res.status(200).json(user);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  },
+  getSwapByAddress: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const address = req.params.address;
+      const user = await userRepository.findOneBy({ address });
+      if (!user) return res.status(403).json('no such user');
+      let swaps = await swapRepository.findAndCount({
+        where: [{ seller: address }, { buyer: address }],
+      });
+      const result = {
+        address: address,
+        totalSwapCount: swaps[1],
+        swaps: swaps[0],
+      };
+      return res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  },
+  getTransactionsByAddress: async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const address = req.params.address;
+      const user = await userRepository.findOneBy({ address });
+      if (!user) return res.status(403).json('no such user');
+      let txQuery = transactionRepository
+        .createQueryBuilder('transactions')
+        .select('transactions')
+        .innerJoin(Swaps, 'swaps', 'swaps.swapId = transactions.swapId')
+        .innerJoin(
+          Users,
+          'users',
+          'users.address = swaps.seller OR users.address = swaps.buyer',
+        )
+        .where(`users.address = "${address}"`);
+      const result = await txQuery.getManyAndCount();
+      return res.status(200).json({
+        address,
+        totaltransactionCount: result[1],
+        transactions: result[0],
+      });
     } catch (err) {
       console.error(err);
       next(err);
--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -6,7 +6,20 @@ const userRouter = express.Router();
 
 userRouter.post('/my', isLoggedIn, userController.postMine);
 userRouter.get('/my', isLoggedIn, userController.getMine);
+userRouter.get(
+  '/my/transactions',
+  isLoggedIn,
+  userController.getMyTransactions,
+);
+userRouter.get('/my/swaps', isLoggedIn, userController.getMySwaps);
+
 userRouter.get('/:address', userController.getByAddress);
+userRouter.get('/:address/swaps', userController.getSwapByAddress);
+userRouter.get(
+  '/:address/transactions',
+  userController.getTransactionsByAddress,
+);
+
 userRouter.get('/', userController.getAll);
 
 export default userRouter;
