87fab9c9d7d8c92472ad79fec0a1b5611f61cabb
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-02-07 23:00:34 +0900

    Fix: change default value of migration file

--- contract/migrations/2_create_sample_swaps.js
+++ contract/migrations/2_create_sample_swaps.js
@@ -1,25 +1,40 @@
 // this script migrates EWEToken
 const CDS = artifacts.require('CDS');
 const PriceOracleMock = artifacts.require('PriceOracleMock');
+const FUSD = artifacts.require('FUSD');
 
 const defaultHostSetting = true;
-const defaultInitAssetPrice = 100;
-const defaultClaimPrice = 80;
-const defaultLiquidationPrice = 60;
-const defaultSellerDeposit = 400;
-const defaultPremium = 4;
+const defaultInitAssetPrice = 25000;
+const defaultClaimPrice = 21250;
+const defaultLiquidationPrice = 20000;
+const defaultSellerDeposit = 50000;
+const defaultPremium = 750;
 const defaultPremiumInterval = 60 * 10; // 10 minutes
 const defaultPremiumRounds = 12; // total lifecycle of test cds is 2hrs
-const defaultBuyerDeposit = defaultPremium * 3;
+const defaultBuyerDeposit = defaultPremium * (3 + 1);
+const defaultTokenFaucet = 10 ** 8;
 
 let currentSwapId;
 module.exports = async function (deployer, network, accounts) {
   console.log(`Triggering Initial TXs ON : ** ${network.toUpperCase()} **`);
   try {
     const priceOracleMock = await PriceOracleMock.deployed();
+    // token faucet
+    const fusd = await FUSD.deployed();
+    await fusd.transfer(accounts[1], defaultTokenFaucet, { from: accounts[0] });
+    await fusd.transfer(accounts[2], defaultTokenFaucet, { from: accounts[0] });
+    await fusd.transfer(accounts[3], defaultTokenFaucet, { from: accounts[0] });
+    await fusd.transfer(accounts[4], defaultTokenFaucet, { from: accounts[0] });
+
     // case1: account[2] create, account[1] accepts
+    // 이제는 create, accept하려면 transfer 선행되야함... buyer 호출의 경우 4달분 cds ca로
     const cds = await CDS.deployed();
     await cds.setOracle(priceOracleMock.address);
+    await cds.setToken(fusd.address);
+
+    await fusd.transfer(cds.address, defaultPremium * 4);
+    console.log(fusd.balanceOf(cds.address));
+
     await cds.create(
       defaultHostSetting,
       defaultInitAssetPrice,
