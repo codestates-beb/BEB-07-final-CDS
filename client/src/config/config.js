4b013ff1e92062e82a0613b44a95e5d570323479
Duhoon <eclip6@naver.com>
2023-02-08 15:50:11 +0900

    fix: contract 주소 재변경

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -5,7 +5,7 @@ export default {
     'https://nodeauction.42msnsnsfoav6.ap-northeast-2.cs.amazonlightsail.com',
 
   // contract address
-  contractAddr: '0x7DA44B6d75bB2654dF773AF00FF370139C9CAB79',
+  contractAddr: '0xfFB97a65fbdD6E267CC8E0aEeF7b4C49a7dF3A4C',
   oracleAddr: '0x2aC585EE9D26c614F8EB56007cecdbCC57B3Fbc2',
   erc20Addr: '0xd84c59CE9cC30d816Ad9927b45E14A1CCe224CDe',
 
