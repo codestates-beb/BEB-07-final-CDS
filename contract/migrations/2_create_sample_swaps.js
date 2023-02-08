d108751b212b1a553cc9480ce0ad695b6bcb512e
bbabi0901 <69451349+bbabi0901@users.noreply.github.com>
2023-02-08 12:44:44 +0900

    Fix: add method approve before create/accept, delete test case of payPremium

--- contract/migrations/2_create_sample_swaps.js
+++ contract/migrations/2_create_sample_swaps.js
@@ -18,7 +18,9 @@ let currentSwapId;
 module.exports = async function (deployer, network, accounts) {
   console.log(`Triggering Initial TXs ON : ** ${network.toUpperCase()} **`);
   try {
-    const priceOracleMock = await PriceOracleMock.deployed();
+    const priceOracleMock = await PriceOracleMock.deployed(
+      defaultInitAssetPrice,
+    );
     // token faucet
     const fusd = await FUSD.deployed();
     await fusd.transfer(accounts[1], defaultTokenFaucet, { from: accounts[0] });
@@ -26,15 +28,15 @@ module.exports = async function (deployer, network, accounts) {
     await fusd.transfer(accounts[3], defaultTokenFaucet, { from: accounts[0] });
     await fusd.transfer(accounts[4], defaultTokenFaucet, { from: accounts[0] });
 
-    // case1: account[2] create, account[1] accepts
+    // settings
     const cds = await CDS.deployed();
     await cds.setOracle(priceOracleMock.address);
     await cds.setToken(fusd.address);
 
-    await fusd.transfer(cds.address, defaultBuyerDeposit, {
+    // case1: account[2] create, account[1] accepts
+    await fusd.approve(cds.address, defaultBuyerDeposit, {
       from: accounts[2],
     });
-    console.log(fusd.balanceOf(cds.address));
     await cds.create(
       defaultHostSetting,
       defaultInitAssetPrice,
@@ -48,7 +50,7 @@ module.exports = async function (deployer, network, accounts) {
     );
     [currentSwapId] = await cds.getSwapId();
 
-    await fusd.transfer(cds.address, defaultSellerDeposit, {
+    await fusd.approve(cds.address, defaultSellerDeposit, {
       from: accounts[1],
     });
     await cds.accept(defaultInitAssetPrice, currentSwapId, {
@@ -56,7 +58,7 @@ module.exports = async function (deployer, network, accounts) {
     });
     console.log('case 1 created!');
     // case2: account[2] create and cancel
-    await fusd.transfer(cds.address, defaultBuyerDeposit, {
+    await fusd.approve(cds.address, defaultBuyerDeposit, {
       from: accounts[2],
     });
     await cds.create(
@@ -75,8 +77,8 @@ module.exports = async function (deployer, network, accounts) {
 
     console.log('case 2 created!');
     // case3 : account[4] creates and nobody accepts
-    await fusd.transfer(cds.address, defaultBuyerDeposit, {
-      from: accounts[2],
+    await fusd.approve(cds.address, defaultBuyerDeposit, {
+      from: accounts[4],
     });
     await cds.create(
       defaultHostSetting,
@@ -93,7 +95,7 @@ module.exports = async function (deployer, network, accounts) {
     console.log('case 3 created!');
     // case4: account[3] creates and account[1] accepts
     // after price dropped below claim price, account[3] claimes
-    await fusd.transfer(cds.address, defaultBuyerDeposit, {
+    await fusd.approve(cds.address, defaultBuyerDeposit, {
       from: accounts[3],
     });
     await cds.create(
@@ -108,7 +110,7 @@ module.exports = async function (deployer, network, accounts) {
       { from: accounts[3] },
     );
     [currentSwapId] = await cds.getSwapId();
-    await fusd.transfer(cds.address, defaultSellerDeposit, {
+    await fusd.approve(cds.address, defaultSellerDeposit, {
       from: accounts[1],
     });
     await cds.accept(defaultInitAssetPrice, currentSwapId, {
@@ -120,7 +122,7 @@ module.exports = async function (deployer, network, accounts) {
     console.log('case 4 created!');
     // case5: account[2] creates and account[3] accepts
     // after price dropped below liquidation price, account[2] claimes
-    await fusd.transfer(cds.address, defaultBuyerDeposit, {
+    await fusd.approve(cds.address, defaultBuyerDeposit, {
       from: accounts[3],
     });
     await cds.create(
@@ -135,7 +137,7 @@ module.exports = async function (deployer, network, accounts) {
       { from: accounts[3] },
     );
     [currentSwapId] = await cds.getSwapId();
-    await fusd.transfer(cds.address, defaultSellerDeposit, {
+    await fusd.approve(cds.address, defaultSellerDeposit, {
       from: accounts[1],
     });
     await cds.accept(defaultInitAssetPrice, currentSwapId, {
@@ -147,7 +149,7 @@ module.exports = async function (deployer, network, accounts) {
     console.log('case 5 created!');
     // case6: account[1] creates and account[3] accepts
     // account[1] closes swap
-    await fusd.transfer(cds.address, defaultBuyerDeposit, {
+    await fusd.approve(cds.address, defaultBuyerDeposit, {
       from: accounts[1],
     });
     await cds.create(
@@ -162,7 +164,7 @@ module.exports = async function (deployer, network, accounts) {
       { from: accounts[1] },
     );
     [currentSwapId] = await cds.getSwapId();
-    await fusd.transfer(cds.address, defaultSellerDeposit, {
+    await fusd.approve(cds.address, defaultSellerDeposit, {
       from: accounts[3],
     });
     await cds.accept(defaultInitAssetPrice, currentSwapId, {
@@ -173,7 +175,8 @@ module.exports = async function (deployer, network, accounts) {
     console.log('case 6 created!');
     // case7: account[4] creates and account[2] accepts
     // account[4] pays single round premium
-    await fusd.transfer(cds.address, defaultBuyerDeposit, {
+    /*
+    await fusd.approve(cds.address, defaultBuyerDeposit, {
       from: accounts[4],
     });
     await cds.create(
@@ -188,17 +191,21 @@ module.exports = async function (deployer, network, accounts) {
       { from: accounts[4] },
     );
     [currentSwapId] = await cds.getSwapId();
-    await fusd.transfer(cds.address, defaultSellerDeposit, {
+
+    await fusd.approve(cds.address, defaultSellerDeposit, {
       from: accounts[2],
     });
     await cds.accept(defaultInitAssetPrice, currentSwapId, {
       from: accounts[2],
     });
+
     await fusd.approve(cds.address, defaultPremium, { from: accounts[4] });
-    await cds.payPremium(currentSwapId, {
+    const payPremium = await cds.payPremium(currentSwapId, {
       from: accounts[4],
     });
+    console.log(payPremium.logs);
     console.log('case 7 created!');
+  */
   } catch (err) {
     console.error(err);
   }
