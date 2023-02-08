5c69248bfcd45b6117d5a243020910bfc7f86d52
Duhoon <eclip6@naver.com>
2023-02-08 11:56:43 +0900

    fix: ERC20 컨트랙트 주소 더미값

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -7,7 +7,7 @@ export default {
   // contract address
   contractAddr: '0x7BfeBb3e6f3bCf4890c8A2fde0b3e2675B410B9f',
   oracleAddr: '0x2aC585EE9D26c614F8EB56007cecdbCC57B3Fbc2',
-  erc20Addr: '',
+  erc20Addr: '0x000000000000000000000000000000000000',
 
   // market price API
   coinGeckoAPI:
