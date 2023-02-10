73b834904d19aaa40bbe4ff742cbce3c6a3cfae6
Duhoon <eclip6@naver.com>
2023-02-10 18:50:26 +0900

    fix: 발표 컨트랙트 주소 고정

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -5,9 +5,9 @@ export default {
     'https://nodeauction.42msnsnsfoav6.ap-northeast-2.cs.amazonlightsail.com',
 
   // contract address
-  contractAddr: '0xc6B1a8aB70943F89F5c37eC93fC74A11E5831fA1',
-  oracleAddr: '0x2aC585EE9D26c614F8EB56007cecdbCC57B3Fbc2',
-  erc20Addr: '0x91fc3196557c0e3A747961Eb2C100A397843a2B4',
+  contractAddr: '0x6Fd9329553bfC1cF0A8459e502571d133767694b',
+  oracleAddr: '0x63AeE035a950875179befa3d298948B0F6061AcF',
+  erc20Addr: '0xbE4983558F83cF015bE7a68F7F3fE591C151c2b9',
 
   // market price API
   coinGeckoAPI:
